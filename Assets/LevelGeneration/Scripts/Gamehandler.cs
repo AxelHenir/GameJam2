e81b4b8f6c81be3d2b4a89f3fd0ccd54b7fd840e
AxelHenir <axelhenirmail@gmail.com>
2023-11-07 23:08:19 -0500

    Room with wall

--- Assets/LevelGeneration/Scripts/Gamehandler.cs
+++ Assets/LevelGeneration/Scripts/Gamehandler.cs
@@ -5,38 +5,11 @@ using UnityEngine;
 public class Gamehandler : MonoBehaviour
 {
 
-    // Declare the container of all spawned rooms
-    List<GameObject> spawnedRooms = new List<GameObject>();
+    public GameObject[] barracksPrefabs; // An array of room prefabs to choose from.
+    public GameObject[] quartersPrefabs; // An array of room prefabs to choose from.
 
-    // Declare the container of all available slots
-    List<GameObject> availableRooms = new List<GameObject>();
-
-    // Declare list of all prefabs that can be spawned
-    public GameObject[] roomPrefabs;
     public Transform[] startingPositions;
     public Transform[] endPositions;
-
-
-    // Spawn the first room at 0,0
-
-    // For this and each subsequent room spawn,
-
-    // Check that room's tags for opening type
-    // If open above, add (Room.x, Room.y+1, opening down)  to availableRooms
-    // If open below, add (Room.x, Room.y-1, opening up)  to availableRooms
-    // If open left, add (Room.x-1, Room.y, opening right)  to availableRooms
-    // If open left, add (Room.x+1, Room.y, opening left)  to availableRooms
-
-    // Randomly select an element in availableRooms, it will be chosen to spawn in.
-    // Randomly select and appropriate room to spawn from roomPrefabs, according to the tag.
-    // Add the room to spawnedRooms and remove the room from availableRooms
-
-    // Once the number of rooms to spawn surpasses or is equal to the number of avaialable rooms, add only rooms with 1 exit, so as not to overgenerate.
-
-
-
-
-
 
 
     public GameObject characterPrefab; // The character prefab you want to spawn.
@@ -90,19 +63,31 @@ public class Gamehandler : MonoBehaviour
 
     }
 
-    void generateDungeon(){
+    void generateDungeon(int gridWidth, int gridHeight, Transform rootPosition, GameObject[] roomPrefabs){
+
+        for (int x = 0; x < gridWidth; x++)
+        {
+            for (int y = 0; y < gridHeight; y++)
+            {
+                Vector3 spawnPosition = new Vector3(rootPosition.x * 32, rootPosition.y * 18, 0);
+                int randomRoomIndex = Random.Range(0, roomPrefabs.Length);
+                GameObject randomRoomPrefab = roomPrefabs[randomRoomIndex];
+                Instantiate(randomRoomPrefab, spawnPosition, Quaternion.identity, transform);
+            }
+        }
+
         // generate the start
-        int randStartingPos = Random.Range(0,startingPositions.Length); //randomize the start position
-        transform.position = startingPositions[randStartingPos].position; //insert the coordinate of the chosen position
-        Instantiate(roomPrefabs[0], transform.position, Quaternion.identity); //place the room or any specific room we want with coordinate and no rotation
+        // int randStartingPos = Random.Range(0,startingPositions.Length); //randomize the start position
+        // transform.position = startingPositions[randStartingPos].position; //insert the coordinate of the chosen position
+        // Instantiate(roomPrefabs[0], transform.position, Quaternion.identity); //place the room or any specific room we want with coordinate and no rotation
 
-        Debug.Log("inital room done");
+        // Debug.Log("inital room done");
 
-        int randEndPos = Random.Range(0, endPositions.Length); //randomize the start position
-        transform.position = endPositions[randEndPos].position; //insert the coordinate of the chosen position
-        Instantiate(roomPrefabs[1], transform.position, Quaternion.identity); //place the room or any specific room we want with coordinate and no rotation
+        // int randEndPos = Random.Range(0, endPositions.Length); //randomize the start position
+        // transform.position = endPositions[randEndPos].position; //insert the coordinate of the chosen position
+        // Instantiate(roomPrefabs[1], transform.position, Quaternion.identity); //place the room or any specific room we want with coordinate and no rotation
 
-        Debug.Log("end room done");
+        // Debug.Log("end room done");
 
 
     }
