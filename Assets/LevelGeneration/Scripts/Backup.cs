2d65ed1bf62c527d1b560410bd43d08bf0299bfc
AxelHenir <axelhenirmail@gmail.com>
2023-11-07 22:11:28 -0500

    BackupIsLive

--- Assets/LevelGeneration/Scripts/Backup.cs
+++ Assets/LevelGeneration/Scripts/Backup.cs
@@ -1,96 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Backup : MonoBehaviour
+public class RandomRoomGridSpawner : MonoBehaviour
 {
-    // Declare the container of all spawned rooms
-    List<GameObject> spawnedRooms = new List<GameObject>();
+    public GameObject[] roomPrefabs; // An array of room prefabs to choose from.
+    public int gridWidth = 5;        // Number of columns.
+    public int gridHeight = 5;       // Number of rows.
 
-    // Declare the container of all available slots
-    List<GameObject> availableRooms = new List<GameObject>();
-
-    // Declare list of all prefabs that can be spawned
-    public GameObject[] roomPrefabs;
-
-    // Maximum number of rooms to spawn
-    public int maxRoomsToSpawn = 10;
-    
-    // Counter for the number of spawned rooms
-    private int spawnedRoomCount = 0;
-
-    
-    
-    // Spawn the first room at 0,0
     void Start()
     {
-        GameObject firstRoom = Instantiate(roomPrefabs[0], Vector3.zero, Quaternion.identity);
-        spawnedRooms.Add(firstRoom);
-        spawnedRoomCount++;
-
-        // Add the adjacent rooms to availableRooms
-        AddAdjacentRooms(Vector2.zero, firstRoom);
+        SpawnRandomRoomGrid();
     }
 
-    // Function to add adjacent rooms to availableRooms
-    void AddAdjacentRooms(Vector2 position, GameObject room)
+    void SpawnRandomRoomGrid()
     {
-
-        // Check if the maximum room limit has been reached
-        if (spawnedRoomCount >= maxRoomsToSpawn)
-            return;
-            
-        // Check if the room has openings in each direction
-        if (room.CompareTag("OpenAbove"))
+        for (int x = 0; x < gridWidth; x++)
         {
-            Vector2 abovePosition = position + Vector2.up;
-            AddRoomToAvailable(abovePosition, "OpeningDown");
-        }
-        if (room.CompareTag("OpenBelow"))
-        {
-            Vector2 belowPosition = position + Vector2.down;
-            AddRoomToAvailable(belowPosition, "OpeningUp");
-        }
-        if (room.CompareTag("OpenLeft"))
-        {
-            Vector2 leftPosition = position + Vector2.left;
-            AddRoomToAvailable(leftPosition, "OpeningRight");
-        }
-        if (room.CompareTag("OpenRight"))
-        {
-            Vector2 rightPosition = position + Vector2.right;
-            AddRoomToAvailable(rightPosition, "OpeningLeft");
-        }
-    }
-
-    // Function to add a room to availableRooms
-    void AddRoomToAvailable(Vector2 position, string tag)
-    {
-        // Check if a room already exists at the given position
-        if (availableRooms.Exists(room => (Vector2)room.transform.position == position))
-            return;
-
-        // Find a suitable prefab to spawn based on the tag
-        GameObject roomPrefab = FindRoomPrefabByTag(tag);
-
-        if (roomPrefab != null)
-        {
-            GameObject room = Instantiate(roomPrefab, position, Quaternion.identity);
-            availableRooms.Add(room);
-        }
-    }
-
-    // Function to find a room prefab by tag
-    GameObject FindRoomPrefabByTag(string tag)
-    {
-        foreach (GameObject prefab in roomPrefabs)
-        {
-            if (prefab.CompareTag(tag))
+            for (int y = 0; y < gridHeight; y++)
             {
-                return prefab;
+                Vector3 spawnPosition = new Vector3(x * 32, y * 18, 0);
+                int randomRoomIndex = Random.Range(0, roomPrefabs.Length);
+                GameObject randomRoomPrefab = roomPrefabs[randomRoomIndex];
+                Instantiate(randomRoomPrefab, spawnPosition, Quaternion.identity, transform);
             }
         }
-        return null;
     }
 }
-
