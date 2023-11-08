b7df63a64a6e58be8cab1222f05fbdd29ae7f65e
Noe235 <sannoemie@outlook.com>
2023-11-07 22:33:49 -0500

    noe

--- Assets/LevelGeneration/Scripts/Gamehandler.cs
+++ Assets/LevelGeneration/Scripts/Gamehandler.cs
@@ -13,21 +13,22 @@ public class Gamehandler : MonoBehaviour
 
     // Declare list of all prefabs that can be spawned
     public GameObject[] roomPrefabs;
+    public Transform[] startingPositions;
 
     // Spawn the first room at 0,0
 
     // For this and each subsequent room spawn,
 
-        // Check that room's tags for opening type
-        // If open above, add (Room.x, Room.y+1, opening down)  to availableRooms
-        // If open below, add (Room.x, Room.y-1, opening up)  to availableRooms
-        // If open left, add (Room.x-1, Room.y, opening right)  to availableRooms
-        // If open left, add (Room.x+1, Room.y, opening left)  to availableRooms
+    // Check that room's tags for opening type
+    // If open above, add (Room.x, Room.y+1, opening down)  to availableRooms
+    // If open below, add (Room.x, Room.y-1, opening up)  to availableRooms
+    // If open left, add (Room.x-1, Room.y, opening right)  to availableRooms
+    // If open left, add (Room.x+1, Room.y, opening left)  to availableRooms
 
-        // Randomly select an element in availableRooms, it will be chosen to spawn in.
-        // Randomly select and appropriate room to spawn from roomPrefabs, according to the tag.
-        // Add the room to spawnedRooms and remove the room from availableRooms
-    
+    // Randomly select an element in availableRooms, it will be chosen to spawn in.
+    // Randomly select and appropriate room to spawn from roomPrefabs, according to the tag.
+    // Add the room to spawnedRooms and remove the room from availableRooms
+
     // Once the number of rooms to spawn surpasses or is equal to the number of avaialable rooms, add only rooms with 1 exit, so as not to overgenerate.
 
 
@@ -51,6 +52,7 @@ public class Gamehandler : MonoBehaviour
         disbaleControls();
 
         // Generate the dungeon in its entirety
+
         generateDungeon();
 
         // Fill dungeon with collectibles
@@ -86,7 +88,12 @@ public class Gamehandler : MonoBehaviour
     }
 
     void generateDungeon(){
+        // generate the start
+        int randStartingPos = Random.Range(0,startingPositions.Length); //randomize the start position
+        transform.position = startingPositions[randStartingPos].position; //insert the coordinate of the chosen position
+        Instantiate(roomPrefabs[0], transform.position, Quaternion.identity); //place the room or any specific room we want with coordinate and no rotation
 
+        Debug.Log("inital room done");
     }
 
     void generateCollectibles(){
